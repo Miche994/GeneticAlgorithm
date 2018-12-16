e38972c5cd9b7abdff93c3401f5f2201751e4a3c
Simone Magnani <simone.magnani3@studio.unibo.it>
2018-12-16 18:27:27 +0100

    Added fields to geneticAlgorithm class

--- geneticAlgorithm.cpp
+++ geneticAlgorithm.cpp
@@ -7,14 +7,43 @@ using namespace std;
 GeneticAlgorithm::GeneticAlgorithm(Database *db, int seconds) {
 	this->instance = db;
 	this->timeout = seconds;
+	this->populationSize = db->nIndexes * 0.001 / 100; //Temporary solution
 }
 
 void GeneticAlgorithm::run() {
 	int startTime = time(NULL);
 	printf("Tempo d'inizio: %d\n", startTime);
     fflush(stdout);
+    populationGeneration();
 	while(time(NULL) - startTime < this->timeout) {
-
+		solutionSetSelection();
+		childrenGeneration();
+		localSearch();
+		populationUpdate();
 	}
 	printf("Tempo di fine : %ld\n", time(NULL));
+}
+
+int GeneticAlgorithm::fitnessElaboration(bool *vectorToEvaluate) {
+	return 0;
+}
+
+void GeneticAlgorithm::populationGeneration() {
+	return;
+}
+
+void GeneticAlgorithm::solutionSetSelection() {
+	return;
+}
+
+void GeneticAlgorithm::childrenGeneration() {
+	return;
+}
+
+void GeneticAlgorithm::localSearch() {
+	return;
+}
+
+void GeneticAlgorithm::populationUpdate() {
+	return;
 }
--- geneticAlgorithm.h
+++ geneticAlgorithm.h
@@ -12,8 +12,21 @@ public:
 	GeneticAlgorithm(Database *db, int seconds);
 	void run();
 private:
+	Database *instance;	
+	bool **children;
+	bool **population;
+	bool *bestSolution;
+	bool *parents;		//parents[populationSize]
+	int *fitnessVector;
+	int bestObjFunc;
+	int populationSize;
 	int timeout;
-	Database *instance;
+	int fitnessElaboration(bool *vectorToEvaluate);	 //compute the fitness value of the solution
+	void populationGeneration(); //fills the **population matrix && fills *fitnessVector
+	void solutionSetSelection(); //fills the *parents vector
+	void childrenGeneration();	 //fills the **children matrix
+	void localSearch();			 //improves **children and call fitnessElaboration() of new solutions
+	void populationUpdate();	 //updates **population by taking the best children && fills (updates) *fitnessVector
 };
 
 #endif
