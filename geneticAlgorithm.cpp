6dc6f5d3c20272b2640da8d1f32d677ba1d4c3ab
Frank-92 <francescopalmieri1992@gmail.com>
2018-12-18 18:33:09 +0100

    FitnessEvaluation 1st version

--- geneticAlgorithm.cpp
+++ geneticAlgorithm.cpp
@@ -20,12 +20,65 @@ void GeneticAlgorithm::run() {
 		childrenGeneration();
 		localSearch();
 		populationUpdate();
+		/*vector to make a try*/
+		//bool vectorToEvaluate[50] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 };
+		//fitnessElaboration(vectorToEvaluate);
 	}
 	printf("Tempo di fine : %ld\n", time(NULL));
 }
 
 int GeneticAlgorithm::fitnessElaboration(bool *vectorToEvaluate) {
-	return 0;
+	int vecSize = instance->nConfigurations;
+    bool *vectConfigActive = new bool[vecSize];
+    for(int i=0;i<instance->nConfigurations;i++){
+        vectConfigActive[i]=1;
+    }
+    int costInMem=0;
+
+    for(int i=0;i<instance->nIndexes;i++){
+       if(vectorToEvaluate[i]==true){costInMem=costInMem+instance->indexesMemory[i];}
+	}
+
+	if (costInMem>instance->totalMemory){
+		delete [] vectConfigActive;
+		cout << "Infeasible\n";
+		return -1;
+	}
+   
+    for(int i=0;i<instance->nIndexes;i++){
+        if(vectorToEvaluate[i]==0){
+            for(int j=0;j<instance->nConfigurations;j++){
+                if(vectConfigActive[j]==1){
+                    if(instance->configurationIndexMatrix[i][j]==1)
+						vectConfigActive[j]=0;
+                }
+            }
+        }
+    }
+
+    int totalQueryGain=0;
+    for(int i=0;i<instance->nQueries;i++){
+		int maxQueryGain=0;
+        for(int j=0;j<instance->nConfigurations;j++){
+           if(vectConfigActive[j]==1){
+			if(instance->configurationGainMatrix[i][j]>maxQueryGain)
+                maxQueryGain=instance->configurationGainMatrix[i][j];
+          }
+        }
+
+		totalQueryGain+=maxQueryGain;
+    }
+
+    int buildIndexCost=0;
+    for(int i=0;i<instance->nIndexes;i++){
+		if(vectorToEvaluate[i]==true){buildIndexCost=buildIndexCost+instance->indexesCost[i];}
+	}
+
+	delete [] vectConfigActive;
+	cout << totalQueryGain - buildIndexCost;
+	cout<<"\n";
+	return (totalQueryGain-buildIndexCost);
+
 }
 
 void GeneticAlgorithm::populationGeneration() {
