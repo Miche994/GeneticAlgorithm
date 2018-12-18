8128303a11a44c34efd0325e880114151863cbb0
s41m0n <simonemagnani.96@gmail.com>
2018-12-18 17:53:42 +0100

    populationUpdate() and populationGeneration() created

--- geneticAlgorithm.cpp
+++ geneticAlgorithm.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
 #include <time.h>
+#include <math.h>
 #include "geneticAlgorithm.h"
 
 using namespace std;
 
 GeneticAlgorithm::GeneticAlgorithm(Database *db, int seconds) {
+	srand(time(0));
 	this->instance = db;
 	this->timeout = seconds;
-	this->populationSize = db->nIndexes * 0.001 / 100; //Temporary solution
+	this->populationSize = pow(2,db->nIndexes) * 0.0000000000001; //Temporary solution
+	this->population = (bool**) malloc (this->populationSize * sizeof(bool*));
+	if (this->population == NULL)
+		exit(1);
+	for (int i = 0; i < populationSize; i++){
+		this->population[i] = (bool*) malloc (db->nIndexes * sizeof(bool));
+		if (this->population[i] == NULL)
+			exit(1);
+	}
+	this->children = (bool**) malloc (this->populationSize * sizeof(bool*));
+	for (int i = 0; i < populationSize; i++){
+		this->population[i] = (bool*) malloc (db->nIndexes * sizeof(bool));
+		if (this->population[i] == NULL)
+			exit(1);
+	}
+	this->fitnessVector = (int*) malloc (db->nIndexes * sizeof(int));
 }
 
 void GeneticAlgorithm::run() {
@@ -29,6 +46,9 @@ int GeneticAlgorithm::fitnessElaboration(bool *vectorToEvaluate) {
 }
 
 void GeneticAlgorithm::populationGeneration() {
+	for(int i = 0; i < this->populationSize; i++)
+		for(int j = 0; j < this->instance->nIndexes; j++)
+			this->population[i][j] = rand() & 1;
 	return;
 }
 
@@ -45,5 +65,10 @@ void GeneticAlgorithm::localSearch() {
 }
 
 void GeneticAlgorithm::populationUpdate() {
+	bool **tmp = this->population;
+	this->population = this->children;
+	this->children = tmp;
+	for(int i = 0; i < this->populationSize; i++)
+		this->fitnessVector[i] = fitnessElaboration(this->population[i]);
 	return;
 }
